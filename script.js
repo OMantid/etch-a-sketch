b03c444859a07ee6a569fadccdf389f6da12d7a8
Bongo <64612244+OMantid@users.noreply.github.com>
2020-08-09 00:46:04 -0700

    Added function and eventlisteners to create and color the grid

--- script.js
+++ script.js
@@ -2,10 +2,27 @@
 // If we use CSS Grid the columns and rows would be 6.25%
 const container = document.getElementById('container');
 
-container.style.display = 'grid';
+function createGrid() {
+    container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+    container.style.gridTemplateRows = 'repeat(16, 1fr)';
 
-for( let i=0; i < 256; i++){
-    let div = document.createElement('div');
-    //div.textContent = i;
-    container.appendChild(div);
+    for (let i=0; i < 256; i++) {
+        let div = document.createElement('div');
+        div.classList.add('block');
+        div.style.backgroundColor = '#dcdcdc';
+        container.appendChild(div);
+    }
+}
+
+createGrid();
+
+const block = document.getElementsByClassName('block');
+
+for (const foo of block) {
+    foo.addEventListener('mouseover', () => {
+        foo.style.backgroundColor = '#696969';
+    });
+    foo.addEventListener('mouseout', () => {
+        foo.style.backgroundColor = '#696969';
+    });
 }
